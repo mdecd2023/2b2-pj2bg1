fd63dcefeaa4b6c32330f800997f6875b2c1f406
40923246 <40923246@gm.nfu.edu.tw>
2023-05-18 05:04:05 +0800

    4cars

--- downloads/瀏覽器中計分板設計 (.7z含場景與控制程式)/zmq_bubbleRob_4_blue.py
+++ downloads/瀏覽器中計分板設計 (.7z含場景與控制程式)/zmq_bubbleRob_4_blue.py
@@ -6,12 +6,14 @@ client = RemoteAPIClient('localhost', 23000)
 
 print('Program started')
 sim = client.getObject('sim')
+
+# start
 sim.startSimulation()
 print('Simulation started')
 
 def setBubbleRobVelocity(leftWheelVelocity, rightWheelVelocity):
-    leftMotor = sim.getObject('/leftMotor2')
-    rightMotor = sim.getObject('/rightMotor2')
+    leftMotor = sim.getObject('/leftMotor4')
+    rightMotor = sim.getObject('/rightMotor4')
     sim.setJointTargetVelocity(leftMotor, leftWheelVelocity)
     sim.setJointTargetVelocity(rightMotor, rightWheelVelocity)
 
@@ -24,13 +26,13 @@ setBubbleRobVelocity(0.0, 0.0)
 # use keyborad to move BubbleRob
 
 while True:
-    if keyboard.is_pressed('w'):
+    if keyboard.is_pressed('i'):
         setBubbleRobVelocity(5.0, 5.0)
-    elif keyboard.is_pressed('s'):
+    elif keyboard.is_pressed('k'):
         setBubbleRobVelocity(-3.0, -3.0)
-    elif keyboard.is_pressed('a'):
+    elif keyboard.is_pressed('j'):
         setBubbleRobVelocity(-2.0, 2.0)
-    elif keyboard.is_pressed('d'):
+    elif keyboard.is_pressed('l'):
         setBubbleRobVelocity(2.0, -2.0)
     elif keyboard.is_pressed('q'):
         # stop simulation
